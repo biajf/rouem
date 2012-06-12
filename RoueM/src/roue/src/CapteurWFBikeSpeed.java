716e03b80f9eb9c137299684de659d76c8241992
bianco.jfrancois@gmail.com <bianco.jfrancois@gmail.com@55f54550-396c-896b-83f9-f67da2c97503>
2012-06-12 13:54:50 +0000

    Capteur connexion OK

--- RoueM/src/roue/src/CapteurWFBikeSpeed.java
+++ RoueM/src/roue/src/CapteurWFBikeSpeed.java
@@ -1,13 +1,65 @@
 package roue.src;
 
+import android.content.Context;
+
 import com.wahoofitness.api.WFHardwareConnector;
 import com.wahoofitness.api.WFHardwareConnectorTypes.WFSensorType;
 import com.wahoofitness.api.comm.WFBikeCadenceConnection;
+import com.wahoofitness.api.comm.WFBikeSpeedConnection;
+import com.wahoofitness.api.comm.WFFootpodConnection;
 import com.wahoofitness.api.data.WFBikeCadenceData;
+import com.wahoofitness.api.data.WFFootpodData;
 
 
 
 public class CapteurWFBikeSpeed extends CapteurWF {
 
+	public CapteurWFBikeSpeed(Context context) {
+		super(context);
+		mSensorType = WFSensorType.WF_SENSORTYPE_FOOTPOD;
+		// TODO Auto-generated constructor stub
+	}
 
+	public enum BikeCadenceData {
+		DEVICE_ID,
+		CADENCE,
+	}
+	
+
+	public enum FootData {
+		DEVICE_ID,
+		SPEED,
+		PACE,
+		DISTANCE,
+		STRIDE_COUNT,
+		CADENCE
+	};
+	
+	public WFFootpodConnection getFootpodConnection() {
+		WFFootpodConnection retVal = null;
+		if ( mConnection instanceof WFFootpodConnection ) {
+			retVal = (WFFootpodConnection)mConnection;
+		}
+		
+		return retVal;
+	}
+	
+	public void updateDisplay() {
+		
+		WFFootpodConnection footpodConnection = getFootpodConnection();
+		if (footpodConnection != null && footpodConnection.isConnected()) {
+			// display connection info.
+			//printValue(FootData.DEVICE_ID, "Device ID:  " + footpodConnection.getDeviceNumber()); 
+			
+            // display standard SDM data.
+			WFFootpodData fpData = footpodConnection.getFootpodData();
+			if (fpData != null) {
+			/*	printValue(FootData.SPEED, "Speed:  " + fpData.getFormattedSpeed(true)); 
+				printValue(FootData.PACE, "Pace:  " + fpData.getFormattedPace(true)); 
+				printValue(FootData.DISTANCE, "Distance:  " + fpData.getFormattedDistance(true)); 
+				printValue(FootData.STRIDE_COUNT, "Strides:  " + fpData.accumulatedStride); 
+				printValue(FootData.CADENCE, "Cadence:  " + fpData.getFormattedCadence(true));*/
+			}
+		}
+	}
 }
--- RoueM/src/roue/src/RoueMActivity.java
+++ RoueM/src/roue/src/RoueMActivity.java
@@ -1,89 +1,247 @@
 package roue.src;
 
-import java.util.Random;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class RoueMActivity extends Activity implements SensorEventListener {
+
+import com.wahoofitness.api.WFAntException;
+import com.wahoofitness.api.WFAntNotSupportedException;
+import com.wahoofitness.api.WFAntServiceNotInstalledException;
+import com.wahoofitness.api.WFDisplaySettings;
+import com.wahoofitness.api.WFHardwareConnector;
+import com.wahoofitness.api.WFHardwareConnectorTypes.WFAntError;
+import com.wahoofitness.api.WFHardwareConnectorTypes.WFHardwareState;
+import com.wahoofitness.api.WFHardwareConnectorTypes.WFSensorType;
+import com.wahoofitness.api.comm.WFBikePowerConnection;
+import com.wahoofitness.api.comm.WFConnectionParams;
+import com.wahoofitness.api.comm.WFSensorConnection;
+import com.wahoofitness.api.comm.WFSensorConnection.WFSensorConnectionStatus;
+
+
+public class RoueMActivity extends Activity implements WFHardwareConnector.Callback {
+	private WFHardwareConnector mHardwareConnector;
+	private static final String TAG = "Test";
+	private WFConnectionParams connectParams;
+	private WFSensorConnection mConnection;
+	private static final int DIALOG_ID_SETTINGS = 0;
+	private static final int DIALOG_ID_BP_CALIBRATE = 1;
+	private CapteurWFBikeSpeed sensor1;
+	private TextView distance;
 	
-	boolean compte = true;
-	private SensorManager sensorManager;
-	private RoueMesureuse roue ;
-	float distparcouru = 0;
-	float s1, s2, s3;
-	EditText distance = null;
+	private enum MyMenu {
+		MENU_NONE,
+		MENU_SETTINGS,
+		MENU_BP_CALIBRATION,
+		MENU_EXIT,      
+	};
 	
-    /** Called when the activity is first created. */
+	
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.main);
+	        distance = (TextView)findViewById(R.id.textView1);
+	        Context context = this.getApplicationContext();
+	        int statusVisibility = TextView.INVISIBLE;
+	        String statusText = "";
+	         
+	        // check for ANT hardware support.
+	        antConnect(context, savedInstanceState, statusText, statusVisibility);
+	        
+
+	   }
+	 
+	 public void start(View v){
+		 
+		 
+		 
+		
+		 //distance.setText(""+tutu);
+	 }
+	 
+	 public void stop(View v)
+	 {
+		 sensor1.disconnectSensor();
+	 }
+	public void antConnect(Context context,Bundle savedInstanceState,String statusText,int statusVisibility)
+	{
+		if (WFHardwareConnector.hasAntSupport(context)) {
+        	
+	        try {
+	        	boolean bResumed = false;
+	        	
+	        	// attempt to retrieve the previously suspended WFHardwareConnector instance.
+	        	//
+	        	// see the onRetainNonConfigurationInstance method.
+	        	mHardwareConnector = (WFHardwareConnector) getLastNonConfigurationInstance();
+	        	if (mHardwareConnector != null) {
+	        		// attempt to resume the WFHardwareConnector instance.
+	        		if (!(bResumed = mHardwareConnector.resume(this))) {
+	        			// if the WFHardwareConnector instance failed to resume,
+	        			// it must be re-initialized.
+	        			mHardwareConnector.connectAnt();
+	        		}
+	        	}
+	        	
+	        	// if there is no suspended WFHardwareConnector instance,
+	        	// configure the singleton instance.
+	        	else {
+			         // get the hardware connector singleton instance.
+			        mHardwareConnector = WFHardwareConnector.getInstance(this, this);
+					mHardwareConnector.connectAnt();
+	        	}
+		        
+		        // restore connection state only if the previous
+		        // WFHardwareConnector instance was not resumed.
+		        if (!bResumed) {
+			        // the connection state is cached in the state
+			        // bundle (onSaveInstanceState).  this is used to
+			        // restore previous connections.  if the Bundle
+			        // is null, no connections are configured.
+			        mHardwareConnector.restoreInstanceState(savedInstanceState);
+		        }
+		        
+		        // configure the display settings.
+		        //
+		        // this demonstrates how to use the display
+		        // settings.  if this step is skipped, the
+		        // default settings will be used.
+		        WFDisplaySettings settings = mHardwareConnector.getDisplaySettings();
+		        settings.staleDataTimeout = 5.0f;          // seconds, default = 5
+		        settings.staleDataString = "--";           // string to display when data is stale, default = "--"
+		        settings.useMetricUnits = true;            // display metric units, default = false
+		        settings.bikeWheelCircumference = 2.07f;   // meters, default = 2.07
+		        settings.bikeCoastingTimeout = 3.0f;       // seconds, default = 3    
+		        mHardwareConnector.setDisplaySettings(settings);
+		        
+		       statusVisibility = TextView.INVISIBLE;
+	        }
+	        catch (WFAntNotSupportedException nse) {
+	        	// ANT hardware not supported.
+	        	statusVisibility = TextView.VISIBLE;
+	        	statusText = "ANT not supported.";
+	        }
+	        catch (WFAntServiceNotInstalledException nie) {
+
+				//Toast installNotification = Toast.makeText(context, this.getResources().getString( R.string.Notify_Service_Required), Toast.LENGTH_LONG);
+				//installNotification.show();
+
+				// open the Market Place app, search for the ANT Radio service.
+				mHardwareConnector.destroy();
+				mHardwareConnector = null;
+				WFHardwareConnector.installAntService(this);
+
+				// close this app.
+				finish();
+	        }
+			catch (WFAntException e) {
+				statusVisibility = TextView.VISIBLE;
+				statusText = "ANT initialization error.";
+			}
+       }
+        else {
+        	// ANT hardware not supported.
+        	statusVisibility = TextView.VISIBLE;
+        	statusText = "ANT not supported.";
+        }
+        
+        if(!this.isFinishing())
+        {
+            //setContentView(R.layout.landscape_grid);
+            initControls();
+        	//mAntStateText.setVisibility(statusVisibility);
+        	//mAntStateText.setText(statusText);
+        }
+	}
+	
+    ///////////////////////////////////////////////////////////////////////////
+	//
+	// WFHardwareConnector.Callback Implementation
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        roue = new RoueMesureuse(this);
-        roue.initialiser(SENSOR_SERVICE);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_NORMAL);  
-        distance = (EditText) findViewById(R.id.distance);
-      //  sensorManager.getOrientation(R, values);
+	public void hwConnAntError(WFAntError error) {
+		switch (error) {
+			case WF_ANT_ERROR_CLAIM_FAILED:
+	        	//mAntStateText.setVisibility(TextView.VISIBLE);
+	        	//mAntStateText.setText("ANT radio in use.");
+				//mHardwareConnector.forceAntConnection(getResources().getString(R.string.app_name));
+				break;
+		}
+	}
+	
+   @Override
+	public void hwConnConnectionRestored() {
+    	//mBikeCadence.restoreConnectionState();
     }
 
-    public void start(View v) throws InterruptedException {
-    	compte = true;
-    	while(compte){
-    		distparcouru = distance(2*(float)(Math.PI*0.4)) + distparcouru;
-    		distance.setText(Float.toString(distparcouru));
-    		Thread.sleep(10000);
-    		
-    	}
-    	//tToast("Start");
-    }
-    
-    public void pause(View v) {
-    	tToast("Pause");
-    }
-    
-    public void stop(View v) {
-    	compte = false;
-    	tToast("Stop");
-    
-    }
-    
-    public void reset(View v) {
-    	tToast("Reset");
-    }
+	@Override
+	public void hwConnConnectedSensor(WFSensorConnection sensorConnection) {
+//		mTextDevId.setText(Integer.toString(sensorConnection.getDeviceNumber()));
+	}
 
-    private void tToast(String s) {
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_LONG/2;
-        Toast toast = Toast.makeText(context, s, duration);
-        toast.show();
-    }
-    
-    public float distance(float circonference){
-    	double RPM = Math.random()*10; 
-    	
-    	return (Math.abs((int)RPM)*circonference);
-    }
-
-	public void onAccuracyChanged(Sensor arg0, int arg1) {
-		// TODO Auto-generated method stub
+	@Override
+	public void hwConnDisconnectedSensor(WFSensorConnection sensorConnection) {
 		
 	}
 
-	public void onSensorChanged(SensorEvent event) {
-		// TODO Auto-generated method stub
-		s1 = event.values[0];
-		s2 = event.values[1];
-		s3 = event.values[2];
+	@Override
+	public void hwConnHasData() {
+		Log.d(TAG, "hwConnHasData");
+		sensor1.updateDisplay();
+		WFSensorConnectionStatus tutu = sensor1.connectSensor();
+		 
+		 distance.setText(""+sensor1.getFootpodConnection().getDeviceNumber());
+		 //mBikeCadence.updateDisplay();
 	}
+
+	public void hwConnStateChanged(WFHardwareState state) {
+		switch (state) {
+			case WF_HARDWARE_STATE_DISABLED:
+	        	//mAntStateText.setVisibility(TextView.VISIBLE);
+	        	if (WFHardwareConnector.hasAntSupport(this)) {
+	        		//mAntStateText.setText("HW Connector DISABLED.");
+	        	}
+	        	else {
+	        		//mAntStateText.setText("ANT Radio NOT supported.");
+	        	}
+				break;
+				
+			case WF_HARDWARE_STATE_SERVICE_NOT_INSTALLED:
+	        	//mAntStateText.setVisibility(TextView.VISIBLE);
+	        	//mAntStateText.setText("ANT Radio Service NOT installed.");
+				break;
+				
+			case WF_HARDWARE_STATE_SUSPENDED:
+	        	//mAntStateText.setVisibility(TextView.VISIBLE);
+	        	//mAntStateText.setText("HW Connector SUSPENDED.");
+	        	break;
+	        	
+			case WF_HARDWARE_STATE_READY:
+			default:
+	        	//mAntStateText.setVisibility(TextView.INVISIBLE);
+				break;
+		}
+	}
+    
+    ///////////////////////////////////////////////////////////////////////////
+	//
+	
+	public void initControls(){
+		
+		sensor1 = new CapteurWFBikeSpeed(getBaseContext());
+		 sensor1.initControl(mHardwareConnector);
+	}
+	    
 
 }
